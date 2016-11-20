9d91abf74e4ad30d5bf39eb87bb5f94537df318d
Olga Filipova <chudaol@gmail.com>
2016-11-20 23:42:32 +0100

    fixed e2e test

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -3,17 +3,12 @@
 
 module.exports = {
   'default e2e tests': function (browser) {
-    // automatically uses dev Server port from /config.index.js
-    // default: http://localhost:8080
-    // see nightwatch.conf.js
-    const devServer = browser.globals.devServerURL
-
     browser
-      .url(devServer)
+      .url('http://localhost:8081')
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
+      .assert.elementPresent('.nav.nav-tabs')
+      .assert.elementPresent('.tab-content')
+      .assert.containsText('h2', 'Groceries')
       .end()
   }
 }
